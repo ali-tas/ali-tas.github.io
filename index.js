58c55f6314d3244f9d5ebcb3f2929564a3a40597
ali77 <tasali8e@gmail.com>
2026-06-01 22:10:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+var password = "code1";
 
+function passcheck() {
+
+if(document.getElementById("pass1").value != password) {
+alert("Falsch!");
+}
+
+if(document.getElementById("pass1").value == password) {
+}
+
+}
