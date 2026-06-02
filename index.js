99caac29b4f76cba9a02841b33693220682f04c7
ali77 <tasali8e@gmail.com>
2026-06-02 15:25:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-var password = "code1";
+var password1 = "code1";
+var password2 = "code2";
 
-function passcheck() {
+function passcheck1() {
 
-if(document.getElementById("pass1").value != password) {
+if(document.getElementById("pass1").value != password1) {
 alert("Falsch!");
 return false;
 }
 
-if(document.getElementById("pass1").value == password) {
+if(document.getElementById("pass1").value == password1) {
+return true;
+}
 }
 
+function passcheck2() {
+
+if(document.getElementById("pass2").value != password2) {
+alert("Falsch!");
+return false;
+}
+
+if(document.getElementById("pass2").value == password2) {
+return true;
+}
 }
