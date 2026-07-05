9bad6444c953d3153663bf6424e88423ea5da99e
ali77 <tasali8e@gmail.com>
2026-07-06 00:46:14 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,173 @@
+var passwordF = "385";
+var password1 = "471";
+var password2 = "5134";
+var password3 = "11,6";
+var password4 = "11";
+var password5 = "2548";
+var password6 = "14";
+
+var passwordF_2 = "905";
+var password1_2 = "368,85";
+var password2_2 = "26,6";
+var password3_2 = "542,18";
+var password4_2 = "2343";
+var password5_2 = "7,46";
+
+
+function passcheckF() {
+
+if(document.getElementById("passF").value != passwordF) {
+alert("Falsch. Überprüfe dein Ergebnis.");
+return false;
+}
+
+if(document.getElementById("passF").value == passwordF) {
+return true;
+}
+}
+
+function passcheck1() {
+
+if(document.getElementById("pass1").value != password1) {
+alert("Falsch. Überprüfe dein Ergebnis.");
+return false;
+}
+
+if(document.getElementById("pass1").value == password1) {
+return true;
+}
+}
+
+function passcheck2() {
+
+if(document.getElementById("pass2").value != password2) {
+alert("Falsch. Überprüfe dein Ergebnis.");
+return false;
+}
+
+if(document.getElementById("pass2").value == password2) {
+return true;
+}
+}
+
+function passcheck3() {
+
+if(document.getElementById("pass3").value != password3) {
+alert("Falsch. Überprüfe dein Ergebnis.");
+return false;
+}
+
+if(document.getElementById("pass3").value == password3) {
+return true;
+}
+}
+
+function passcheck4() {
+
+if(document.getElementById("pass4").value != password4) {
+alert("Falsch. Überprüfe dein Ergebnis.");
+return false;
+}
+
+if(document.getElementById("pass4").value == password4) {
+return true;
+}
+}
+
+function passcheck5() {
+
+if(document.getElementById("pass5").value != password5) {
+alert("Falsch. Überprüfe dein Ergebnis.");
+return false;
+}
+
+if(document.getElementById("pass5").value == password5) {
+return true;
+}
+}
+
+function passcheck6() {
+
+if(document.getElementById("pass6").value != password6) {
+alert("Falsch. Überprüfe dein Ergebnis.");
+return false;
+}
+
+if(document.getElementById("pass6").value == password6) {
+return true;
+}
+}
+
+
+
+function passcheckF_2() {
+
+if(document.getElementById("passF_2").value != passwordF_2) {
+alert("Falsch. Überprüfe dein Ergebnis.");
+return false;
+}
+
+if(document.getElementById("passF_2").value == passwordF_2) {
+return true;
+}
+}
+
+function passcheck1_2() {
+
+if(document.getElementById("pass1_2").value != password1_2) {
+alert("Falsch. Überprüfe dein Ergebnis.");
+return false;
+}
+
+if(document.getElementById("pass1_2").value == password1_2) {
+return true;
+}
+}
+
+function passcheck2_2() {
+
+if(document.getElementById("pass2_2").value != password2_2) {
+alert("Falsch. Überprüfe dein Ergebnis.");
+return false;
+}
+
+if(document.getElementById("pass2_2").value == password2_2) {
+return true;
+}
+}
+
+function passcheck3_2() {
+
+if(document.getElementById("pass3_2").value != password3_2) {
+alert("Falsch. Überprüfe dein Ergebnis.");
+return false;
+}
+
+if(document.getElementById("pass3_2").value == password3_2) {
+return true;
+}
+}
+
+function passcheck4_2() {
+
+if(document.getElementById("pass4_2").value != password4_2) {
+alert("Falsch. Überprüfe dein Ergebnis.");
+return false;
+}
+
+if(document.getElementById("pass4_2").value == password4_2) {
+return true;
+}
+}
+
+function passcheck5_2() {
+
+if(document.getElementById("pass5_2").value != password5_2) {
+alert("Falsch. Überprüfe dein Ergebnis.");
+return false;
+}
+
+if(document.getElementById("pass5_2").value == password5_2) {
+return true;
+}
+}
